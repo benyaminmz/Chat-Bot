e633e431a1b245d4cd4d098afb5ffbcdc9626e87
benyaminmz <mooazenzadeh99@gmail.com>
2025-04-06 11:15:13 +0330

    Update main.py

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 logger = logging.getLogger(__name__)
 
 # توکن و آدرس‌ها
-TOKEN = '7764880184:AAEAp5oyNfB__Cotdmtxb9BHnWgwydRN0ME'
+TOKEN = '5287864667:AAGs4CJ0DW1qhfsA447Kvyz-WJFlUbIeBh4'
 IMAGE_API_URL = 'https://pollinations.ai/prompt/'
 TEXT_API_URL = 'https://text.pollinations.ai/'
 URL = "https://platopedia.com/items"
